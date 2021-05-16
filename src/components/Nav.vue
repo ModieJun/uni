7588f467d17bc51d26568b6ce3ada534b390cb47
modiejun <junjielin8888@gmail.com>
2021-05-16 21:52:29 +0800

    init

---
+++ src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>  
+    <div class="">
+        <nav class="w-full h-16 ">
+                <div class="container mx-auto h-full p-2 md:px-10 justify-around flex flex-row items-center"> 
+                        <router-link to="/">Home</router-link>
+                        <router-link to="/about">About</router-link>
+                </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
