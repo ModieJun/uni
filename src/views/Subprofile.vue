0edc10970d3bfdaa61f44e1f785538c49e36f2ed
modiejun <junjielin8888@gmail.com>
2021-05-17 11:54:00 +0800

    implement profile and subprofile skeletons

---
+++ src/views/Subprofile.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-col w-full p-10">
+        <h2>Subprofile:{{this.$route.params}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Subprofile"
+}
+</script>
+
+<style>
+
+</style>
