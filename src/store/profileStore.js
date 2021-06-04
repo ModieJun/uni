2c1d4b9e080ff167f5278e880de1163067d94eee
modiejun <junjielin8888@gmail.com>
2021-06-04 15:24:59 +0800

    styling: fix button color styling

--- src/store/profileStore.js
+++ src/store/profileStore.js
@@ -1,44 +1,49 @@
-import {db,Firebase} from '../firebase_conf';
-import { vuexfireMutations,firestoreAction } from 'vuexfire'
+import { db } from '../firebase_conf';
+import firebase from 'firebase'
+import { vuexfireMutations, firestoreAction } from 'vuexfire'
 
 const profileStore = {
-    state:{
-        profile:{},
-        subprofiles:[]
+    state: {
+        profile: null,
+        subprofiles: []
     },
-    mutations:{
-        addMainprofile:function({state},newProfile){
-            state.profile=newProfile;
+    mutations: {
+        addMainProfile(state, newProfile) {
+            state.profile = newProfile;
         },
-        addsubprofile:function({state},newSubprofile){
-            state.subprofile.appendChild(newSubprofile);
+        addSubprofile(state, newSubprofile) {
+            state.subprofile.push(newSubprofile);
         },
         ...vuexfireMutations
     },
-    actions:{
-        addMainProfile:function({commit},user,profile){
+    actions: {
+        addMainProfile: async function ({ commit }, data) {
+            let newProfile = { profile: data.profile, created_at: firebase.firestore.FieldValue.serverTimestamp() };
             // add the main profile to the cloud
-            db.collection("users").doc(user.uid)
-            .collection("mainprofile")
-            .add({profile,created_at:Firebase.firestore.FieldValue.serverTimestamp()})
-            .then((data)=>{
-                //get changes -> update the local store
-                console.log(data);
-                commit("addMainProfile",data);
-            },(error)=>{
-                console.log("Error: ",error);
-            })
+            let result = await db.collection("users").doc(data.user.uid)
+                .collection("mainprofile")
+                .add(newProfile)
+                .then((data) => {
+                    //get changes -> update the local store
+                    console.log(data);
+                    commit("addMainProfile", newProfile.profile);
+                    return { success: true };
+                }, (error) => {
+                    console.log("Error: ", error);
+                    return { success: false, error: error };
+                });
+            return result;
         },
-        addSubprofile:function({commit},user,subprofile){
-            db.collection("users").doc(user.uid)
-            .collection("subprofiles")
-            .add({subprofile,created_at:Firebase.firestore.FieldValue.serverTimestamp()})
-            .then((data)=>{
-                console.log(data);
-                commit("addSubprofile",data);
-            },error=>{
-                console.log("Error: ",error);
-            })
+        addSubprofile: function ({ commit }, data) {
+            db.collection("users").doc(data.user.uid)
+                .collection("subprofiles")
+                .add({ subprofie: data.subprofile, created_at: firebase.firestore.FieldValue.serverTimestamp() })
+                .then((ref) => {
+                    console.log(ref);
+                    commit("addSubprofile", data.subprofie);
+                }, error => {
+                    console.log("Error: ", error);
+                })
         },
         // removeSubprofile:function({commit},user,profile){
 
@@ -46,32 +51,32 @@ const profileStore = {
         // updateSubprofile:function({commit},user,profile){
 
         // },
-        bindUserProfile:function(user){
-            firestoreAction(({bindFirestoreRef})=>{
-                return bindFirestoreRef('profile',db.collection("users").doc(user.uid).collection('mainprofile'));
+        bindUserProfile: function (user) {
+            firestoreAction(({ bindFirestoreRef }) => {
+                return bindFirestoreRef('profile', db.collection("users").doc(user.uid).collection('mainprofile'));
             })
         },
-        bindSubprofile: function(user){
-            firestoreAction(({bindFirestoreRef})=>{
-                return bindFirestoreRef('subprofiles',db.collection("users").doc(user.uid).collection('subprofiles'))
+        bindSubprofile: function (user) {
+            firestoreAction(({ bindFirestoreRef }) => {
+                return bindFirestoreRef('subprofiles', db.collection("users").doc(user.uid).collection('subprofiles'))
             });
         },
-        unbindUserProfileAndSubprofiles:function(){
-            firestoreAction(({unbindFirestoreRef})=>{
+        unbindUserProfileAndSubprofiles: function () {
+            firestoreAction(({ unbindFirestoreRef }) => {
                 unbindFirestoreRef('profile');
                 unbindFirestoreRef('subprofiles');
             })
         }
     },
-    getters:{
-        profile: (state)=>{
+    getters: {
+        profile: (state) => {
             return state.profile;
         },
-        subprofiles:(state)=>{
+        subprofiles: (state) => {
             return state.subprofiles;
         },
-        subprofileByID:(state)=>(id)=>{
-            return state.subprofiles.find(subprofile=>subprofile.id==id)
+        subprofileByID: (state) => (id) => {
+            return state.subprofiles.find(subprofile => subprofile.id == id)
         }
     }
 }
